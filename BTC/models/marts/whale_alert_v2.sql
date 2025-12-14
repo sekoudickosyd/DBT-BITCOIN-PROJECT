5855042d13540718ec1e1ba55da982c85e05a96f
sekou dicko <sekoudickosyd@gmail.com>
2025-12-14 04:18:52 +0100

    add new evols

---
+++ BTC/models/marts/whale_alert_v2.sql
@@ -0,0 +1,23 @@
+with whale as (
+    select 
+        address,
+        sum(output_value) as total_sent,
+        count(*) as tx_count
+
+    from {{ ref('stg_btc_transactions') }}
+
+    where output_value > 10 
+
+    group by address 
+
+    order by total_sent desc 
+)
+
+select 
+    address,
+    total_sent,
+    tx_count
+
+from whale 
+
+order by total_sent desc 
